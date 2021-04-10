63509c6bc54ad2adf8e567b42ba653b20e198979
Josey C. Wörmann <jcw05@gmx.de>
2021-04-10 21:30:37 +0200

    Add files via upload

---
+++ benchmark.v
@@ -0,0 +1,28 @@
+import time
+import os
+
+println("This benchmark will measure your computers single-threaded performance.")
+
+mut runs := os.input("How many runs do you want to run?\n>").int()
+println("Starting $runs benchmark runs...")
+
+// Start stopwatch to measure duration of the benchmark
+duration := time.new_stopwatch({})
+
+mut x := 2
+
+runs++
+
+for i := 1; i < runs; i++ {
+	println("Starting run $i ...")
+
+	for j := 0; j < 2000000000; j++ {
+		x = x * 2
+	}
+}
+
+end := duration.elapsed().seconds()
+
+os.write_file("SCORE.txt", "The benchmark took $end.str() seconds to complete.")?
+println("Benchmark finished in $end seconds, your score has been saved in \"SCORE.txt\".")
+// TODO: make result to seconds and keep the nachkommastellen
